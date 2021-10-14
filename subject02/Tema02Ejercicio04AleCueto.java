1300ed9c941b82e3c04fcec783e88ae4375a668a
Ale Cueto Jiménez <alecuetocontacto@gmail.com>
2021-10-14 10:37:00 +0200

    fourth exercise from second subject

---
+++ subject02/Tema02Ejercicio04AleCueto.java
@@ -0,0 +1,14 @@
+/**
+ * Asigna variables y saca por pantalla
+ *
+ * @author Ale Cueto
+ */
+public class Tema02Ejercicio04AleCueto {
+  public static void main(String[] args) {
+    double euros = 100;
+    double pesetas = euros * 166.386;
+    System.out.println(euros + " euro/s son " + pesetas + " pesetas");
+  }
+}
+
+
