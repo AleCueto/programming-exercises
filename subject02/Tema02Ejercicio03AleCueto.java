426f819ba8e3df9ac0f57f4c1be329dd4a8e21c8
Ale Cueto Jiménez <alecuetocontacto@gmail.com>
2021-10-14 10:28:56 +0200

    third exercise from second subject

---
+++ subject02/Tema02Ejercicio03AleCueto.java
@@ -0,0 +1,15 @@
+/**
+ * Asigna variables y saca por pantalla
+ *
+ * @author Ale Cueto
+ */
+public class Tema02Ejercicio03AleCueto {
+  public static void main(String[] args) {
+    String nombre = "Alejandro Cueto Jiménez";
+    String direccion = "C/ Periquito Nº 10";
+    int telefono = 684152927;
+    System.out.println(nombre + "\n" + direccion + "\n"+ telefono);
+  }
+}
+
+
