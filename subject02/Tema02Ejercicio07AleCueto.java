6d867c787b02afa2dd540b55320a8c70949e09fb
Ale Cueto Jiménez <alecuetocontacto@gmail.com>
2021-10-14 10:56:33 +0200

    seventh exercise second subject

---
+++ subject02/Tema02Ejercicio07AleCueto.java
@@ -0,0 +1,14 @@
+/**
+ * Muestra char y string juntas
+ *
+ * @author Ale Cueto
+ */
+public class Tema02Ejercicio07AleCueto {
+  public static void main(String[] args) {
+    char letra1 = 'h', letra2 = 'o', letra3 = 'l', letra4 = 'a';
+    String palabra = "mundo";
+    System.out.println("" + letra1 + letra2 + letra3 + letra4 + " " + palabra);
+  }
+}
+
+
