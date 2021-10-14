85969c3e282f4736fe4fe9df3d62c391df3d9c65
Ale Cueto Jiménez <alecuetocontacto@gmail.com>
2021-10-14 10:40:36 +0200

    fifth exercise from second subject

--- subject02/Tema02Ejercicio05AleCueto.java
+++ subject02/Tema02Ejercicio05AleCueto.java
@@ -3,7 +3,7 @@
  *
  * @author Ale Cueto
  */
-public class Tema02Ejercicio04AleCueto {
+public class Tema02Ejercicio05AleCueto {
   public static void main(String[] args) {
     double euros = 100;
     double pesetas = euros * 166.386;
