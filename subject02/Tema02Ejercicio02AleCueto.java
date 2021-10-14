340a62b40dc396ab05d1e0d8aa9385c24f3b3764
Ale Cueto Jiménez <alecuetocontacto@gmail.com>
2021-10-14 10:22:34 +0200

    second exercise from second subject

---
+++ subject02/Tema02Ejercicio02AleCueto.java
@@ -0,0 +1,13 @@
+/**
+ * Asigna variables, opera y saca por pantalla
+ *
+ * @author Ale Cueto
+ */
+public class Tema02Ejercicio02AleCueto {
+  public static void main(String[] args) {
+    String nombre = "Alejandro Cueto Jiménez";
+    System.out.println(nombre);
+  }
+}
+
+
