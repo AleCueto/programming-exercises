5a4bc8f75343995c151fecdeeefbf832db55a3d0
Ale Cueto Jiménez <alecuetocontacto@gmail.com>
2021-10-14 11:01:20 +0200

    eighth exercise from second subject

---
+++ subject02/Tema02Ejercicio08AleCueto.java
@@ -0,0 +1,14 @@
+/**
+ * Muestra char y asignalas a una string
+ *
+ * @author Ale Cueto
+ */
+public class Tema02Ejercicio08AleCueto {
+  public static void main(String[] args) {
+    char letra1 = 'h', letra2 = 'o', letra3 = 'l', letra4 = 'a';
+    String palabra = "" + letra1 + letra2 + letra3 + letra4;
+    System.out.println(palabra);
+  }
+}
+
+
