ec5525d6fcd89cc9fe0828df9817190086450012
Ale Cueto Jiménez <alecuetocontacto@gmail.com>
2021-10-14 10:15:17 +0200

    first exercise from second subject

---
+++ subject02/Tema02Ejercicio01AleCueto.java
@@ -0,0 +1,18 @@
+/**
+ * Asigna variables, opera y saca por pantalla
+ *
+ * @author Ale Cueto
+ */
+public class Tema02Ejercicio01AleCueto {
+  public static void main(String[] args) {
+    int x = 144;
+    int y = 999;
+    System.out.println("x es igual a " + x + ", y es igual a " + y);
+    System.out.println("la suma de x e y es igual a " + (x + y));
+    System.out.println("la resta de x e y es igual a " + (x - y));
+    System.out.println("la división de x e y es igual a " + ((float)x / (float)y));
+    System.out.println("la multiplicación de x e y es igual a " + (x * y));
+  }
+}
+
+
