2bb9c33456474c894b94e2e2d59ddd4868830261
Ale Cueto Jiménez <alecuetocontacto@gmail.com>
2021-10-14 10:48:53 +0200

    sixth exercise from second subject

---
+++ subject02/Tema02Ejercicio06AleCueto.java
@@ -0,0 +1,14 @@
+/**
+ * Calcula factura a partir de base imponible
+ *
+ * @author Ale Cueto
+ */
+public class Tema02Ejercicio06AleCueto {
+  public static void main(String[] args) {
+    double baseImponible = 100;
+    double iva = 1.21;
+    System.out.println("El total de la factura es " + baseImponible * iva + "€");
+  }
+}
+
+
