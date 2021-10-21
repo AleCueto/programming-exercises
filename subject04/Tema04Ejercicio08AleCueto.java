ae5e40439f60fa6f7ad8d656ff2bdf9325dac84b
Ale Cueto Jiménez <alecuetocontacto@gmail.com>
2021-10-21 09:54:48 +0200

    eigtht exercise from fourth subject

---
+++ subject04/Tema04Ejercicio08AleCueto.java
@@ -0,0 +1,37 @@
+/**
+ * Calcula media aritmética de tres notas y di si está aprobado, no o sobresaliente
+ * 
+ * Utilizaremos la formula √2h/g siendo g = 9.81
+ * 
+ * @author Ale Cueto
+ */
+public class Tema04Ejercicio08AleCueto {
+  public static void main(String[] args) {
+    System.out.println("¡Vamos a calcular la media aritmética de tres notas!");
+    
+    System.out.print("Por favor, indícame cual es la primera nota: ");
+    float nota1 = Float.parseFloat(System.console().readLine());
+    
+    System.out.print("Ahora, indícame cual es la segunda nota: ");
+    float nota2 = Float.parseFloat(System.console().readLine());
+    
+    System.out.print("Por último, indícame cual es la tercera nota: ");
+    float nota3 = Float.parseFloat(System.console().readLine());
+    
+    float media = (nota1 + nota2 + nota3)/ 3;
+    
+    String nota = "";
+    
+    if(media >= 5 && media < 9){
+      nota = "aprovada";
+    } else if (media > 10){
+      nota = "imposible";
+    } else{
+      nota = "suspensa";
+    }
+    
+    
+    System.out.printf("La media aritmética de las notas %.2f %.2f y %.2f es igual a ", nota1, nota2, nota3);
+    System.out.printf("%.2f puntos, eso significa que tu nota es %s", media, nota);
+}
+}
