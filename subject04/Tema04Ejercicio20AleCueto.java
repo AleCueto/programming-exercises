aeddd2b51689d1ff7e884930da1181db559f52e5
Ale Cueto Jiménez <alecuetocontacto@gmail.com>
2021-10-28 09:08:57 +0200

    exercise 20 from subject 04

--- subject04/Tema04Ejercicio20AleCueto.java
+++ subject04/Tema04Ejercicio20AleCueto.java
@@ -7,23 +7,50 @@
 public class Tema04Ejercicio20AleCueto {
   public static void main(String[] args) {
     System.out.println("¡Vamos a ver si un número es capicúo!");
-    System.out.print("Introduce el número del que deseas comprobar si es capicúo: ");
+    System.out.print("Introduce el número del que deseas comprobar si es capicúo, debe ser entero y positivo: ");
     
-    int numero = Integer.parseInt(System.console().readLine());
+    double numero = Double.parseDouble(System.console().readLine());
 
-    int primerNumero = numero / 10000;
-    int segundoNumero = numero / 1000;
-    int tercerNumero = numero / 100;
-    int cuartoNumero = numero / 10;
-    int ultimoNumero = numero % 10;
+    double unidad = numero % 10;
+    double decena = (int)numero / 10 % 10;
+    double centena = (int)numero / 100 % 10;
+    double millar = (int)numero / 1000 % 10;
+    double decenaMillar = (int)numero / 10000 % 10;
 
-    System.out.println("" + tercerNumero);
 
-    
-    if(numero.length() <= 5){
-      
+
+    boolean capicuo = false;
+
+
+    if((int)numero / 10 % 10 > 0){
+      if(decena == unidad){
+        capicuo = true;
+      }
     }
 
-    System.out.println("El número de dígitos del número es " + texto.length());
+    if((int)numero / 100 % 10 > 0){
+      if(centena == unidad){
+        capicuo = true;
+      }
+    }
+
+    if((int)numero / 1000 % 10 > 0){
+      if(millar == unidad && decena == centena){
+        capicuo = true;
+      }
+    }
+
+    if((int)numero / 10000 % 10 > 0){
+      if(decenaMillar == unidad && millar == decena){
+        capicuo = true;
+      }
+    }
+
+    if(capicuo == true){
+      System.out.println("El número " + (int)numero + " es capicúo");
+    } else{
+      System.out.println("El numero " + (int)numero + " no es capicúo");
+    }
+
   }
 }
