0e97b99ed25a60f73830ea898b426d26d807f580
Ale Cueto Jiménez <alecuetocontacto@gmail.com>
2021-11-08 09:25:59 +0100

    exercise 14 from subject 05

---
+++ subject05/Tema05Ejercicio14AleCueto.java
@@ -0,0 +1,29 @@
+/**
+ * ¡Pide base y exponente y hace la operación!
+ * 
+ * 
+ * @author Ale Cueto
+ */
+public class Tema05Ejercicio14AleCueto {
+  public static void main(String[] args) {
+
+    System.out.println("¡Vamos a hacer una portencia!");
+    
+    System.out.print("\nPrimero dame la base: ");
+    double base = Double.parseDouble(System.console().readLine());
+
+    double resultado = base;
+
+
+    System.out.print("Ahora dame el exponente: ");
+    double aux = Double.parseDouble(System.console().readLine());
+
+    int exponente = (int)aux;
+
+    for(int i = 1; i < exponente; i++){
+      resultado = resultado * base;
+    }
+
+    System.out.println("\n" + base + " elevado a " + exponente + " = " + resultado + "\n");
+  }
+}
