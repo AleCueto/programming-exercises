05e9957ccd551297531e9573cb50b334a2e28dec
Ale Cueto Jiménez <alecuetocontacto@gmail.com>
2021-11-15 08:54:41 +0100

    exercise 19 for subject 05

---
+++ subject05/Tema05Ejercicio19AleCueto.java
@@ -0,0 +1,36 @@
+import java.util.EmptyStackException;
+
+/**
+ * ¡vamos a pintar una pirámide con altura determinada por teclado!
+ * 
+ * 
+ * @author Ale Cueto
+ */
+public class Tema05Ejercicio19AleCueto {
+  public static void main(String[] args) {
+    
+    System.out.println("¡vamos a pintar una pirámide con altura determinada por teclado!");
+
+    System.out.print("\nDime el carácter con el que quieres crear la pirámide: ");
+    char caracter = System.console().readLine().charAt(0);
+
+    System.out.print("Ahora dime la altura que quieres que tenga la pirámide: ");
+    long altura = Long.parseLong(System.console().readLine());
+
+    System.out.println("\n");
+    long espacios = (altura * 2 - 1)/2;
+
+    for(int i = 0; i < altura; i++){
+      for(int n = 0; n < espacios - i; n++){
+        System.out.print(" ");
+      }
+      for(int x = 0; x <= i * 2; x++){
+        System.out.print(caracter);
+      }
+      for(int n = 0; n < espacios - i; n++){
+        System.out.print(" ");
+      }
+      System.out.print("\n");
+    }
+  }
+}   
