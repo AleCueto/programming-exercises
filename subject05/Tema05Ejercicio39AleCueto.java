26122151df76c86aa0f252d0b62e8b1823c70b7d
Ale Cueto Jiménez <alecuetocontacto@gmail.com>
2021-12-11 19:20:33 +0100

    exercise 39 from subject 05

---
+++ subject05/Tema05Ejercicio39AleCueto.java
@@ -0,0 +1,27 @@
+/**
+ * ¡Pide un número entero positivo y muestra los factoriales hasta él!
+ * 
+ * 
+ * @author Ale Cueto
+ */
+public class Tema05Ejercicio39AleCueto {
+  public static void main(String[] args) {
+
+    System.out.print("¡Vamos a sacar los números desde el uno a un número y su factorial!: ");
+
+    
+    System.out.print("Por favor, introduce el número: ");
+    int numero = Integer.parseInt(System.console().readLine());
+    int factorial = numero;
+    
+    System.out.print(numero + "! = ");
+
+
+    for (int i = numero; i >= 2 ; i--) {
+      factorial *= i-1;
+    }
+
+    System.out.print(factorial + "\n");
+
+  }
+}   
