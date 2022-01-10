47aa36daf67c0a3a77d791e0c949145fa62aceaa
Ale Cueto Jiménez <alecuetocontacto@gmail.com>
2022-01-10 14:30:42 +0100

    exercise 05 form subject 08

--- subject08/Tema08Ejercicio05AleCueto.java
+++ subject08/Tema08Ejercicio05AleCueto.java
@@ -8,42 +8,28 @@ public class Tema08Ejercicio05AleCueto {
   public static void main(String[] args) throws InterruptedException{
     System.out.print("Vamos a mostrar un array con 20 celdas\n");
 
-    int sumaFila = 0;
-    int sumaColumna[] = new int [5];
+    int mayorCol = 0;
+    int mayorFil = 0;
+    int mayor = 0;
 
-    int array[][] = new int[5][4];
 
-    array[0][0] = 0;
-    array[0][1] = 30;
-    array[0][3] = 2;
-    array[0][2] = 5;
-    array[1][0] = 75;
-    array[1][3] = 0;
-    array[2][2] = -2;
-    array[2][3] = 9;
-    array[2][1] = 11;
+    int array[][] = new int[10][6];
 
     for (int i = 0; i < array.length; i++) {
       for (int j = 0; j < array[0].length; j++) {
-        array[i][j] = (int)(Math.random()*899) + 100;
-      }
-    }
-    
-    for (int i = 0; i < array.length; i++) {
-      for (int j = 0; j < array[0].length; j++) {
-        sumaFila += array[i][j];
-        sumaColumna[j] += array[i][j];
+        array[i][j] = (int)(Math.random()*1000);
+        if(array[i][j] >= mayor){
+          mayor = array[i][j];
+          mayorCol = i;
+          mayorFil = j;
+        }
         System.out.print(array[i][j] + "  ");
       }
-      Thread.sleep(1000);
-      System.out.print("Fila " + i + ": " + sumaFila);
-      sumaFila = 0;
+      System.out.println();
       System.out.println();
     }
 
-    for (int i = 0; i < array.length; i++) {
-      Thread.sleep(1000);
-      System.out.print(sumaColumna[i] + " ");
-    }
+    System.out.println("El número más alto es "+ mayor + " y está en la posición [" + mayorFil + " , " + mayorCol + "]");
+
   }
 }
